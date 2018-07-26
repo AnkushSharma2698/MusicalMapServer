eeee15e89f98ff69356e81c76281f0e7b0243946
Ankush Sharma <ankush1@ualberta.ca>
2018-07-26 11:28:45 -0600

    stuff inside

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: '670271955306-anv29n0bj92aeh90pq9nsp6m27huig60.apps.googleusercontent.com',
+    googleClientSecret: 'DbzQDSWHBLlEHi3fFL0EOcal',
+    spotifyClientID: '4c0a28e096e84acbba11ba37511ff13d',
+    spotifyClientSecret: '84ff70727fdf4c628f4a5164cd1d8893',
+    mongoURI: 'mongodb://admin:password1@ds123171.mlab.com:23171/musik',
+    cookieKey: 'sdfashfdjghjshdhjsgfdskfasddahwreadf'
+};
